373c638bf52187fb73ff7acd5d79d20913326ff3
footclan2k2 <HaWyanHa@Ryans-MacBook-Pro-2.local>
2016-05-19 11:51:44 -0400

    games behind on team function works

--- js/controllers/team.controller.js
+++ js/controllers/team.controller.js
@@ -8,9 +8,16 @@
 		TeamController.$inject = ["TeamService"];
 
 		function TeamController(TeamService) {
+
+
 			console.log('in TeamController');
 			this.team = TeamService.getAllTeams;
+			this.first = TeamService.getAllTeams[0];
 
+			this.gamesBehind = function gamesBehind(team){
+				console.log(this.first, team);
+				return ((this.first.wins - team.wins) + (team.loss - this.first.loss))/2;
+			};
 
 		}
 
